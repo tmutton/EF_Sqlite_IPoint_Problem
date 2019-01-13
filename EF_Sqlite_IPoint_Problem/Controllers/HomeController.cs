eb6376e11216aa0a892cd503d3af568476239c5c
Thomas Mutton <tmutton1@gmail.com>
2019-01-13 13:02:42 +0000

    Add project files.

---
+++ EF_Sqlite_IPoint_Problem/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EF_Sqlite_IPoint_Problem.Models;
+
+namespace EF_Sqlite_IPoint_Problem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EF_Sqlite_IPoint_Problem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EF_Sqlite_IPoint_Problem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
